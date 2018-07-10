31159718ea433b156ca0f4e7698745eb8fce12f2
MeijerR <MeijerR@NLHLP626>
2018-07-10 10:23:04 +0200

    Add project files.

---
+++ Pages/Formulier.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPages.Pages
+{
+    public class FormulierModel : PageModel
+    {
+        public void OnGet()
+        {
+            //var x= Request.Form["companyname"];
+        }
+
+    }
+}
